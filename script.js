0a080fe39c2a9d9bd7337e61f60c86e795f298d0
drearydev <143649090+DrearyDev@users.noreply.github.com>
2023-09-24 06:23:43 -0400

    Make Rock, Paper, or Scissors be chosen randomly by computer

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+function getComputerChoice() {
+    let options = ['Rock', 'Paper', 'Scissors'];
+    let choice = options[Math.floor(Math.random() * options.length)];
+    return choice;
+}
+let choice = getComputerChoice()
+console.log(choice);
